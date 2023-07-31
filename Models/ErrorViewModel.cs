fc156f43338205ea30bf5a058ecef24d9330c24c
sivasuthan manojan <mjcode6@gmail.com>
2023-07-31 09:05:46 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace attaque.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
